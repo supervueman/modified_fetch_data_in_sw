549646404716ae40123f017859927a2a5b05ebff
supervueman <supervueman@gmail.com>
2022-05-14 11:16:12 +0400

    Clear cache

--- sw.js
+++ sw.js
@@ -3,14 +3,21 @@ const testUrl = 'http://localhost:3000/my-test-fetch'
 
 const cacheUrls = ['index.html', '/src/index.js', '/css/styles.css']
 
+const cacheStaticName = 'v1-static'
+
 self.addEventListener('install', async event => {
   console.log('Sw install', event)
-  const cache = await caches.open('v1')
+  const cache = await caches.open(cacheStaticName)
   await cache.addAll(cacheUrls)
 })
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', async event => {
   console.log('Sw activate', event)
+
+  const cacheNames = await caches.keys()
+  await Promise.all(
+    cacheNames.filter(name => name !== cacheStaticName).map(name => caches.delete(name))
+  )
 })
 
 self.addEventListener('fetch', event => {
