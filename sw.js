22d5002ba8a542975ec464330244fccd4482e06b
supervueman <supervueman@gmail.com>
2022-05-14 11:57:35 +0400

    Add network first

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 const url = 'https://jsonplaceholder.typicode.com/posts?_limit=11'
 const testUrl = 'http://localhost:3000/my-test-fetch'
 
-const cacheUrls = ['index.html', '/src/index.js', '/css/styles.css']
+const cacheUrls = ['index.html', '/src/index.js', '/css/styles.css', '/offline.html']
 
-const cacheStaticName = 'v1-static'
+const staticCacheName = 'v1-static'
+const dynamicCacheName = 'v1-dynamic'
 
 self.addEventListener('install', async event => {
   console.log('Sw install', event)
-  const cache = await caches.open(cacheStaticName)
+  const cache = await caches.open(staticCacheName)
   await cache.addAll(cacheUrls)
 })
 
@@ -16,28 +17,31 @@ self.addEventListener('activate', async event => {
 
   const cacheNames = await caches.keys()
   await Promise.all(
-    cacheNames.filter(name => name !== cacheStaticName).map(name => caches.delete(name))
+    cacheNames
+      .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
+      .map(name => caches.delete(name))
   )
 })
 
 self.addEventListener('fetch', event => {
   event.respondWith(async function () {
-    if (event.request.url === url) {
-      const res = await fetch(url)
-      console.log('SW:', res)
-      const data = await res.json()
-      const modifiedData = data.map(d => ({
-        ...d,
-        title: 'Modified title',
-      }))
-      const init = { status: 200, statusText: 'SuperSmashingGreat!' }
+    // if (event.request.url === url) {
+    //   const res = await fetch(url)
+    //   console.log('SW:', res)
+    //   const data = await res.json()
+    //   const modifiedData = data.map(d => ({
+    //     ...d,
+    //     title: 'Modified title',
+    //   }))
+    //   const init = { status: 200, statusText: 'SuperSmashingGreat!' }
 
-      const blob = new Blob([JSON.stringify(modifiedData, null, 2)], {type : 'application/json'})
+    //   const blob = new Blob([JSON.stringify(modifiedData, null, 2)], {type : 'application/json'})
 
-      console.log(data)
+    //   console.log(data)
 
-      return new Response(blob, init)
-    }
+    //   return new Response(blob, init)
+    // }
 
     if (event.request.url === testUrl) {
       console.log(event)
@@ -55,7 +59,13 @@ self.addEventListener('fetch', event => {
       return new Response(blob, init)
     }
 
-    return cacheFirst(event.request)
+    const newUrl = new URL(event.request.url)
+
+    if (newUrl.origin === location.origin) {
+      return cacheFirst(event.request)
+    } else {
+      return networkFirst(event.request)
+    }
   }())
 })
 
@@ -63,4 +73,18 @@ const cacheFirst = async (request) => {
   const cached = await caches.match(request)
 
   return cached ?? fetch(request)
+}
+
+const networkFirst = async (request) => {
+  const cache = await caches.open(dynamicCacheName)
+
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+
+    return response
+  } catch (error) {
+    const cached = await cache.match(request)
+    return cached ?? caches.match('/offline.html')
+  }
 }
